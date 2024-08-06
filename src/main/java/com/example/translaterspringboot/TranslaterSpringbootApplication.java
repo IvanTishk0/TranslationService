96d2db78cb71d40688f5e860fc68e2ea009f020b
mac <ivantishko29@gmail.com>
2024-08-06 15:17:14 +0200

    JDBC improvements

--- src/main/java/com/example/translaterspringboot/TranslaterSpringbootApplication.java
+++ src/main/java/com/example/translaterspringboot/TranslaterSpringbootApplication.java
@@ -1,17 +1,12 @@
 package com.example.translaterspringboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
 @SpringBootApplication
 public class TranslaterSpringbootApplication implements CommandLineRunner {
-
-    @Autowired
-    private TranslationService translationService;
 
     public static void main(String[] args) {
         SpringApplication.run(TranslaterSpringbootApplication.class, args);
